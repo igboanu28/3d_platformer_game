92ae0417bd16615da7a694d718fbb014ac513fa1
Igboanu Uchenna <igboanuuchenna@gmail.com>
2025-05-28 15:23:47 +0100

    Add a weapon script to act as a hit box

--- Assets/_Project/Scripts/CombatSystem/PlayerCombat.cs
+++ Assets/_Project/Scripts/CombatSystem/PlayerCombat.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using UnityEngine;
 using System.Collections.Generic;
+using Platformer;
 
 namespace CombatSystem
 {
@@ -11,28 +12,30 @@ namespace CombatSystem
         float lastComboEnd;
         int combotStep;
         Animator animator;
+        Weapon weapon;
+        [SerializeField] InputReader inputReader;
 
         void Start()
         {
-        
+            animator = GetComponent<Animator>();
         }
 
         void Update()
         {
-        
+            ExitLightAttack();
         }
 
         void OnEnable()
         {
             // Subscribe to input events if using an InputReader
-            // inputReader.LightAttack += OnLightAttack;
+             inputReader.LightAttack += OnlightAttack;
             // inputReader.SpinAttack += OnSpinAttack;
             // inputReader.HeavyAttack += OnHeavyAttack;
         }
         void OnDisable()
         {
             // Unsubscribe from input events
-            // inputReader.LightAttack -= OnLightAttack;
+            inputReader.LightAttack -= OnlightAttack;
             // inputReader.SpinAttack -= OnSpinAttack;
             // inputReader.HeavyAttack -= OnHeavyAttack;
         }
@@ -43,19 +46,33 @@ namespace CombatSystem
                 CancelInvoke("EndCombo");
                 if (Time.time - lastClickedTime >= 0.2f)
                 { 
-                
+                    animator.runtimeAnimatorController = combo[combotStep].animatorOverrideController;
+                    animator.Play("LightAttack", 0, 0);
+                    weapon.damage = combo[combotStep].damage;
+                    combotStep++;
+                    lastClickedTime = Time.time;
+
+                    if (combotStep > combo.Count)
+                    {
+                        combotStep = 0; // Reset combo step if it exceeds the count
+                    }
                 }
             }
         }
 
         void ExitLightAttack()
-        { 
-        
+        {
+            if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.9 && animator.GetCurrentAnimatorStateInfo(0).IsTag("LightAttack"))
+            { 
+                Invoke("EndCombo", 1); // Delay to allow the animation to finish
+            }
         }
 
         void EndCombo()
-        { 
-        
+        {
+            combotStep = 0; // Reset combo step
+            lastComboEnd = Time.time; // Record the end time of the combo
         }
     }
+    
 }
